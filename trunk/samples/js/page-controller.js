0a0d46f463fb89b687641d797e5519c43a8de89d
tuanquynet@gmail.com <tuanquynet@gmail.com@cd537f8d-21dc-9bc7-0f54-8d4668e30786>
2013-03-09 06:07:41 +0000

    - add samples - technical-notes.txt

---
+++ trunk/samples/js/page-controller.js
@@ -0,0 +1,20 @@
+var app = app || (function(){
+	var _this = this;
+
+	var createChildren = function() {
+		
+	}
+
+	var start = function() {		
+
+	}
+
+	return {
+		start: start
+	}
+})();
+
+
+jQuery(document).ready(function(){
+	app.start();
+})
